dc56fc027d94f75462be66e322d498398a5e85ae
gh05t0fluc1f3r <134058189+gh05t0fluc1f3r@users.noreply.github.com>
2025-06-09 15:47:15 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from app.routes import threats, websocket
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(threats.router)
+app.include_router(websocket.router)
+
+@app.get("/")
+def root():
+    return {"message": "Phantom Radar Backend Running"}
